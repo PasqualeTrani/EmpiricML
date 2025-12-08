0f836a0c14bdcc1b225aa9394209c007165eda27
Pasquale Trani <pasquale.trani96@outlook.it>
2025-12-09 00:02:11 +0100

    add eval_pipeline in utils

--- src/empml/utils.py
+++ src/empml/utils.py
@@ -2,8 +2,13 @@ import time
 import functools
 import logging
 from pathlib import Path
+from typing import Dict, Union
 
-import polars as pl  # type: ignore
+import polars as pl 
+import numpy as np
+
+from empml.pipelines import Pipeline
+from empml.base import Metric
 
 # --- Logging Setup ---
 logging.basicConfig(
@@ -14,6 +19,7 @@ logging.basicConfig(
 # ------------------------------------------------------------------------------------------
 # DECORATORS 
 # ------------------------------------------------------------------------------------------
+
 def log_execution_time(func):
     """A decorator that logs start, end, and duration of a function."""
     
@@ -42,3 +48,99 @@ def log_execution_time(func):
 # ------------------------------------------------------------------------------------------
 # FUNCTIONS 
 # ------------------------------------------------------------------------------------------
+
+def relative_performance(minimize : bool, x1 : float, x2 : float) -> float:
+    """
+    Compute the relative performance of a pipeline with score x2 with respect to another of score x1 (reference).
+    The same function can be used to compute overfitting.
+    """
+    if minimize:
+        performance = round(((x1 - x2)/(x1)) * 100 ,2)
+    else:
+        performance = round(((x2 - x1)/(x1)) * 100 ,2)
+
+    return performance
+
+
+@log_execution_time
+def eval_pipeline(
+    pipeline : Pipeline,
+    train : pl.LazyFrame, 
+    valid : pl.LazyFrame, 
+    metric : Metric, 
+    target : str,
+    minimize : bool, 
+    eval_overfitting : bool = True, 
+    store_preds : bool = True, 
+    verbose : bool = True
+) -> Dict[str, Union[float, np.array]]:
+    """
+    Evalute pipeline performance by training on the train dataset and validate the prediction on valid dataset. 
+    """
+    
+    # TRAINING
+    
+    if verbose:
+        logging.info(f'Start training...')
+
+    start_train = time.perf_counter()
+
+    pipeline.fit(train)
+
+    end_train = time.perf_counter()
+    duration_train = round(end_train - start_train, 2)
+
+    if verbose:
+        logging.info(f'✓ Training ended.')
+
+    # INFERENCE 
+
+    if verbose:
+        logging.info(f'Start inference...')
+
+    start_inf = time.perf_counter()
+
+    preds = pipeline.predict(valid)
+
+    end_inf = time.perf_counter()
+    duration_inf = round(end_inf - start_inf, 2)
+
+    if verbose:
+        logging.info(f'✓ Inference ended.')
+
+    # PERFORMANCE ON VALIDATION 
+
+    valid = valid.with_columns(pl.Series(preds).alias('preds'))
+    score = metric.compute_metric(lf = valid, target = target, preds = 'preds')
+
+    # INFERENCE ON TRAINING 
+    if eval_overfitting:
+
+        if verbose:
+            logging.info(f'Start computing overfitting...')
+
+        train = train.with_columns(pl.Series(pipeline.predict(train)).alias('preds'))
+        score_on_train = metric.compute_metric(lf = train, target = target, preds = 'preds') 
+        overfitting = relative_performance(minimize, score, score_on_train)
+
+        if verbose:
+            logging.info(f'✓ Compute overfitting ended.')
+
+    else:
+
+        score_on_train = np.nan
+        overfitting = np.nan
+
+    if not(store_preds):
+        preds = np.nan
+
+    results = {
+        'validation_score' : score, 
+        'train_score' : score_on_train, 
+        'overfitting' : overfitting, 
+        'duration_train' : duration_train, 
+        'duration_inf' : duration_inf, 
+        'preds' : preds
+    }
+
+    return results 
